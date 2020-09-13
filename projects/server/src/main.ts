3c73342542f6e61d51e83008eefea0fc04475a2c
Rajesh <rnagaya@visteon.com>
2020-09-13 20:12:49 +0530

    [server] Add support for graphql server
    This CL adds support for express server
    which parses the graphql schema.
    Added support for lerna based project
    system.
    Added support for apis-documentation
    for defined interfaces which are auto-generated
    under |projects/server/api-documentation|.
    
    Signed-off-by: Rajesh <rajeshnagaiya@gmail.com>

---
+++ projects/server/src/main.ts
@@ -0,0 +1,44 @@
+import * as path from "path"
+import { QueryResolvers } from "./generated/appstore"
+
+const { importSchema } = require('graphql-import');
+const { makeExecutableSchema } = require('graphql-tools');
+const cors = require("cors");
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+
+interface Resolvers {
+  Query : QueryResolvers
+}
+
+const resolvers: Resolvers  = {
+  Query: {
+    queryString: (root, ctx, req) => {
+      return 'world'
+    }
+  }
+}
+
+async function getSchema() {
+  return await importSchema(path.join(__dirname, "../src/schema.graphql"));
+}
+
+function main() {
+  getSchema().then((typeDefs) => {
+    var app = express();
+    app.use(cors());
+    const applicationSchema = makeExecutableSchema({
+        typeDefs,
+        resolvers
+      })
+    app.use('/graphql', graphqlHTTP({
+      schema: applicationSchema,
+      graphiql: true,
+    }));
+    app.listen(4000);
+    console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+  })
+}
+
+main();
